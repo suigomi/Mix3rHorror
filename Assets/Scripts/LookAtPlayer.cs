5661f967c14bfbd97b33918dcb3f344086530a62
rikukasa <80609650+rikukasa@users.noreply.github.com>
2022-03-06 00:08:54 +0900

    LookAtPlayerの変更
    playerにターゲットを入れて、speedで追ってくるスピードを設定

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
+
+    public GameObject player;
+    [SerializeField] private float speed = 0.1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ターゲット方向のベクトル
+        Vector3 relativePos = player.transform.position - this.transform.position;
+
+        // 方向を、回転情報に変換
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        // 回転させる
+        transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, speed);
     }
+
 }
