b1b4e97aaa48077b8ef4c0e2b71f7ca473d69859
rikukasa <80609650+rikukasa@users.noreply.github.com>
2022-03-21 16:29:49 +0900

    環境光の調整
    環境光を消して、反射を弱くした

--- Assets/Scripts/ChangeTime.cs
+++ Assets/Scripts/ChangeTime.cs
@@ -26,12 +26,6 @@ public class ChangeTime : MonoBehaviour
     [SerializeField] private Color dC;
 
 
-    //昼の環境光の強さ
-    [SerializeField] float dayIntensity = 1f;
-    //夜の環境光の強さ
-    [SerializeField] float nightIntensity = 0f;
-    //一回の環境光の変化量
-    [SerializeField] float dI;
 
 
     //何回で真っ暗にするか
@@ -50,13 +44,9 @@ public class ChangeTime : MonoBehaviour
         dayColor = lightObj.GetComponent<Light>().color;
         dC = (dayColor - nightColor) / step;
 
-        dI = (dayIntensity - nightIntensity) / step;
-
         skyBoxCopy = new Material(skyBox);//skyboxのコピー
         RenderSettings.skybox = skyBoxCopy;//skyboxをいれる
 
-        RenderSettings.ambientIntensity = dayIntensity;//環境光の強さ
-        RenderSettings.reflectionIntensity = dayIntensity;//環境光の反射の強さ
     }
 
     // Update is called once per frame
@@ -78,20 +68,13 @@ public class ChangeTime : MonoBehaviour
 
             dayColor -= dC;
 
-            dayIntensity -= dI;
-            dayIntensity = (dayIntensity >= nightIntensity) ? dayIntensity : nightIntensity;//夜より暗くならないように
-
             skyBoxCopy.SetFloat("_Exposure", dayExposure);//skyboxを昼に
             lightObj.GetComponent<Light>().color = dayColor;//lightを昼に
-            RenderSettings.ambientIntensity = dayIntensity;//環境光を昼に
-            RenderSettings.reflectionIntensity = dayIntensity;//環境光の反射を昼に
         }
         else
         {
             skyBoxCopy.SetFloat("_Exposure", nightExposure);
             lightObj.GetComponent<Light>().color = nightColor;
-            RenderSettings.ambientIntensity= nightIntensity;
-            RenderSettings.reflectionIntensity = nightIntensity;
         }
     }
 }
