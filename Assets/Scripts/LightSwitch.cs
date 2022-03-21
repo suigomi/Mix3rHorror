39011b43815c3ddb45aaba6014802cdff338daae
Suigomi <49914693+suigomi@users.noreply.github.com>
2022-03-21 15:42:01 +0900

    オアシス調整＆懐中電灯調整
    調整！

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LightSwitch : MonoBehaviour
+{
+    private void Update()
+    {
+        // 現在のキーボード情報
+        var current = Keyboard.current;
+
+        // キーボード接続チェック
+        if (current == null)
+        {
+            // キーボードが接続されていないと
+            // Keyboard.currentがnullになる
+            return;
+        }
+
+        // Aキーの入力状態取得
+        var aKey = current.aKey;
+
+        // Aキーが押された瞬間かどうか
+        if (aKey.wasPressedThisFrame)
+        {
+            Debug.Log("Aキーが押された！");
+        }
+
+        // Aキーが離された瞬間かどうか
+        if (aKey.wasReleasedThisFrame)
+        {
+            Debug.Log("Aキーが離された！");
+        }
+
+        // Aキーが押されているかどうか
+        if (aKey.isPressed)
+        {
+            Debug.Log("Aキーが押されている！");
+        }
+    }
+}
