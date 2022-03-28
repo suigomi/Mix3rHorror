05061a0c7940eb3e81761ac302b19a6bb455a02d
rikukasa <80609650+rikukasa@users.noreply.github.com>
2022-03-28 22:31:09 +0900

    侵食度(san値)によって物体を表示（テスト）
    赤色の円柱をsan値によって表示
    侵食度はGameManagerのヒエラルキーのsanValueから確認
    昼から夜にするとsan値が-10になる
    Test Objects→Appearance Managerのスクリプトに値とオブジェクトを入れることで、表示の切り替えが可能

---
+++ Assets/Scripts/Appearance.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Appearance : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    [Serializable] struct Pair
+    {
+        public int appearanceValue;//表示する侵食度
+        public GameObject gameObject;//表示するオブジェクト
+    }
+
+
+
+    [SerializeField] List<Pair> objects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeAppearance();
+    }
+
+    public void ChangeAppearance()
+    {
+        foreach(Pair p in objects)
+        {
+            // ※インベントリに入っていない状態の時は除くを付け足す
+            if (p.appearanceValue >= gameManager.sanValue || !gameManager.day)
+            {
+                p.gameObject.SetActive(true);
+            }
+            else
+            {
+                p.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //昼か夜か
+    public bool day = true;
+
+    //侵食度
+    public int sanValue;
+    public int sanValueMax = 100; //初期値が100
+    public int sanValueMin = 0; //0になるとゲームオーバー
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sanValue = sanValueMax;
     }
 
     // Update is called once per frame
@@ -15,4 +23,11 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void SubSunValue(int x) //san値からxだけ引く(Substruct)
+    {
+        sanValue -= x;
+        sanValue = Mathf.Max(sanValue, sanValueMin);
+    }
+
 }
