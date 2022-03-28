e1e4b8ee5641be29274b9faf19dcf386a3fe4cda
rikukasa <80609650+rikukasa@users.noreply.github.com>
2022-03-28 14:40:46 +0900

    Update ChangeTime.cs
    昼と夜のfogの設定

--- Assets/Scripts/ChangeTime.cs
+++ Assets/Scripts/ChangeTime.cs
@@ -19,17 +19,32 @@ public class ChangeTime : MonoBehaviour
     //変更するlight(Colorを変える)
     public GameObject lightObj;
     //昼のColor
-    [SerializeField] Color dayColor;
+    [SerializeField] Color dayColorL;
     //夜のColor
-    [SerializeField] Color nightColor = Color.black;
+    [SerializeField] Color nightColorL = Color.black;
     //一回のlightの変化量
-    [SerializeField] private Color dC;
+    [SerializeField] private Color dCL;
 
+    //昼のfogのdensity
+    [SerializeField] float dayFog;
+    //夜のfogのdensity
+    [SerializeField] float nightFog = 0;
+    //一回のdensityの変化量
+    [SerializeField] float dF;
+
+    //昼のfogのcolor
+    [SerializeField] Color dayColorF;
+    //夜のfogのcolor
+    [SerializeField] Color nightColorF = Color.black;
+    //一回のcolorの変化量
+    [SerializeField] Color dCF;
 
 
 
     //何回で真っ暗にするか
     public int step = 10;
+    //何回夜になったか
+    private int numNight = 0;
 
     //昼か夜か
     [SerializeField] bool day = true;
@@ -41,8 +56,14 @@ public class ChangeTime : MonoBehaviour
         dayExposure = skyBox.GetFloat("_Exposure");
         dE = (dayExposure - nightExposure) / step;
 
-        dayColor = lightObj.GetComponent<Light>().color;
-        dC = (dayColor - nightColor) / step;
+        dayColorL = lightObj.GetComponent<Light>().color;
+        dCL = (dayColorL - nightColorL) / step;
+
+        dayFog = RenderSettings.fogDensity;
+        dF = (dayFog - nightFog) / step;
+
+        dayColorF = RenderSettings.fogColor;
+        dCF = (dayColorF - nightColorF) / step;
 
         skyBoxCopy = new Material(skyBox);//skyboxのコピー
         RenderSettings.skybox = skyBoxCopy;//skyboxをいれる
@@ -60,21 +81,34 @@ public class ChangeTime : MonoBehaviour
 
     public void TimeReversal()
     {
-        day = !day; // 昼と夜の反転 
+        if (numNight <= step)
+        {
+            day = !day; // 昼と夜の反転 
+        }
+
         if (day)
         {
             dayExposure -= dE;
-            dayExposure = (dayExposure >= nightExposure) ? dayExposure : nightExposure;//夜より暗くならないように
-
-            dayColor -= dC;
-
             skyBoxCopy.SetFloat("_Exposure", dayExposure);//skyboxを昼に
-            lightObj.GetComponent<Light>().color = dayColor;//lightを昼に
+
+            dayColorL -= dCL;
+            lightObj.GetComponent<Light>().color = dayColorL;//lightを昼に
+
+            dayFog -= dF;
+            RenderSettings.fogDensity = dayFog;//fogのdensityを昼に
+
+            dayColorF -= dCL;
+            RenderSettings.fogColor = dayColorF;
+            
         }
         else
         {
             skyBoxCopy.SetFloat("_Exposure", nightExposure);
-            lightObj.GetComponent<Light>().color = nightColor;
+            lightObj.GetComponent<Light>().color = nightColorL;//setActiveで切った方が軽いかも
+            RenderSettings.fogDensity = nightFog;//fogを切った方が軽いかも
+            RenderSettings.fogColor = nightColorF;
+
+            numNight++;
         }
     }
 }
