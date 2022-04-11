479db184902da61d0245ce53c9cd99098bc55f29
杉野将史 <sgnmssuioaai@gmail.com>
2022-04-11 23:29:18 +0900

    バイク修理temp

---
+++ Assets/Scripts/suga/RepairBike.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RepairBike : MonoBehaviour
+{
+
+    [SerializeField]
+    AssembleBikeObj[] assembleBikeObjs;
+
+    
+
+
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<BikeParts>().GetPartTag == BikeParts.Part.Wrench)
+        {
+            if (assembleBikeObjs.All(value => value.IsSetted))
+            {
+                foreach (AssembleBikeObj obj in assembleBikeObjs)
+                {
+                    obj.BikePart.Repair();
+                }
+            }
+        }
+    }
+
+}
