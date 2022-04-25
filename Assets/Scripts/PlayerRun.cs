bbfa2e411542d5806975e725c135d74a6130d7af
rikukasa <80609650+rikukasa@users.noreply.github.com>
2022-04-25 21:56:32 +0900

    ダッシュ用のAction追加

---
+++ Assets/Scripts/PlayerRun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+public class PlayerRun : MonoBehaviour
+{
+    private ContinuousMoveProviderBase XrOrign;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XrOrign = GameObject.Find("XR Origin").GetComponent<ContinuousMoveProviderBase>();
+    }
+
+    void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
