95de29f692082819dba328311065826a1284c62c
Suigomi <49914693+suigomi@users.noreply.github.com>
2022-05-02 20:32:14 +0900

    temp commit
    フラッシュライトでゲームオーバーの処理消した

--- Assets/Models/FlashLight/LightSwitch.cs
+++ Assets/Models/FlashLight/LightSwitch.cs
@@ -32,6 +32,6 @@ public class LightSwitch : MonoBehaviour
         Light.SetActive(isActive);
         audioSource.PlayOneShot(audioClips[Light.activeSelf ? 0 : 1]);
 
-        gameManager.GameOver();
+        // gameManager.GameOver();
     }
 }
