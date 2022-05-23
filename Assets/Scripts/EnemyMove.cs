ff6cb18ab642b1fe03d807b77981318242df3f29
Suigomi <49914693+suigomi@users.noreply.github.com>
2022-05-23 21:59:50 +0900

    敵とか殺せるようになりました！
    フー

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("追いかける対象")]
+    private GameObject player;
+    
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // NavMeshAgentを保持しておく
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid) 
+        {
+            // プレイヤーを目指して進む
+            navMeshAgent.destination = player.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour
+{
+	/// <summary>
+	/// パーティクルが他のGameObject(Collider)に当たると呼び出される
+	/// </summary>
+	/// <param name="other"></param>
+	private void OnParticleCollision(GameObject other)
+	{
+		// 当たった相手の色をランダムに変える
+		// other.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other);
+        }
+	}
+}
